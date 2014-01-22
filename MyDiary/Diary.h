ddc87936c6e17b9b32b5e657e6397168a2ca6ecd
jin jianxiong <jinjianxiong@jins-Mac.local>
2014-01-22 21:19:30 +0800

    新增日记类Diary空文件

---
+++ MyDiary/Diary.h
@@ -0,0 +1,13 @@
+//
+//  Diary.h
+//  MyDiary
+//
+//  Created by jin jianxiong on 22/1/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Diary : NSObject
+
+@end
---
+++ MyDiary/Diary.m
@@ -0,0 +1,13 @@
+//
+//  Diary.m
+//  MyDiary
+//
+//  Created by jin jianxiong on 22/1/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import "Diary.h"
+
+@implementation Diary
+
+@end
