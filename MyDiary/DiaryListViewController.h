9f3f3ed540c9fe1e07f60359533f6cdb4f3b022a
jin jianxiong <jinjianxiong@jins-Mac.local>
2014-01-24 10:16:36 +0800

    新增日记列表类

---
+++ MyDiary/DiaryListViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiaryListViewController.h
+//  MyDiary
+//
+//  Created by jin jianxiong on 24/1/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiaryListViewController : UITableViewController
+
+@end
