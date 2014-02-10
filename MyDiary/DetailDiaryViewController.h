413cf1106794aef61a814038a9db29b4c1d3fd62
jin jianxiong <jinjianxiong@jins-Mac.local>
2014-02-10 16:47:54 +0800

    新建 “日记详细页“控制器detailDiaryViewController

---
+++ MyDiary/DetailDiaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailDiaryViewController.h
+//  MyDiary
+//
+//  Created by jin jianxiong on 10/2/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailDiaryViewController : UIViewController
+
+@end
---
+++ MyDiary/DetailDiaryViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailDiaryViewController.m
+//  MyDiary
+//
+//  Created by jin jianxiong on 10/2/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import "DetailDiaryViewController.h"
+
+@interface DetailDiaryViewController ()
+
+@end
+
+@implementation DetailDiaryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
