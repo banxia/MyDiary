e9091015de40bd7e1e5b599344bb2ad58e18682c
jin jianxiong <jinjianxiong@jins-Mac.local>
2014-02-11 10:56:31 +0800

    新建视图控制器CreateDiaryViewController

---
+++ MyDiary/CreateDiaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateDiaryViewController.h
+//  MyDiary
+//
+//  Created by jin jianxiong on 11/2/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateDiaryViewController : UIViewController
+
+@end
---
+++ MyDiary/CreateDiaryViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreateDiaryViewController.m
+//  MyDiary
+//
+//  Created by jin jianxiong on 11/2/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import "CreateDiaryViewController.h"
+
+@interface CreateDiaryViewController ()
+
+@end
+
+@implementation CreateDiaryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
