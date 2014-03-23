5b1f513ecbe6c1f82c4fa3f37b34349671801c00
jjx <jjx@jjxdeMacBook-Pro.local>
2014-03-23 17:48:38 +0800

    新建文件
    新建视图控制器，用于新增日记时添加图片

---
+++ MyDiary/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  MyDiary
+//
+//  Created by jjx on 3/23/14.
+//  Copyright (c) 2014 jjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
